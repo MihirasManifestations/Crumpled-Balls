82418c08f340d409389fe9af7575db9fd808704b
MihirasManifestations <84073794+MihirasManifestations@users.noreply.github.com>
2021-06-02 15:04:57 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,35 @@
+class paper
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true,			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+
+        this.image=loadImage("paper.png");
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperPos=this.body.position;		
+
+			push()
+			translate(paperPos.x, paperPos.y);
+			rotate(this.body.angle)
+			rectMode(CENTER)
+			fill(255)
+			//rect(0,0,this.w, this.h);
+            imageMode(CENTER)
+			image(this.image,0,0,this.w,this.h);
+			pop()
+			
+	}
+
+}
